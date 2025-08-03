13ef5ee3cca8cf2d9e6159b384997a93f36210d3
Maxim Gvozdev <mars63gvozdev@yandex.ru>
2025-08-03 14:29:42 +0400

    add 19.3

--- 19.3/webpack.config.js
+++ 19.3/webpack.config.js
@@ -1,13 +1,10 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.js"),
+  entry: "./index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
-  plugins: [new HtmlWebpackPlugin()],
+  mode: "development",
 };
